978282e6e1508c81cfaade36bd5e5865b3047a14
Alind Xhyra <xhyra.alind@gmail.com>
2022-02-12 13:16:56 +0100

    Added classes skeletons, implementation not yet done

--- pyreport/text.py
+++ pyreport/text.py
@@ -4,7 +4,20 @@ class Text:
         self.section = section
 
     def add_ref(self, ref):
-        self.text.format('\\ref{' + self.section[ref].label + '}')
+        refs = ('\\ref"{"{}"}"'*len(ref)).format(*ref)
+        self.text.format(*refs)
+
+    def add_itemize(self, items):
+        self.text.append('\n\\begin{itemize}')
+        for item in items:
+            self.text.append('\n\\item ' + item)
+        self.text.append('\n\\end{itemize}')
+
+    def add_enumerate(self, items):
+        self.text.append('\n\\begin{enumerate}')
+        for item in items:
+            self.text.append('\n\\item ' + item)
+        self.text.append('\n\\end{enumerate}')
 
     def compile(self):
         return self.text
